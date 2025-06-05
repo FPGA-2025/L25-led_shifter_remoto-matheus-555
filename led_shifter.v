2b6a9cd9a809f9ca5d869b4043cea330a9dc7b17
Matheus de Alencar <sletezes@gmail.com>
2025-06-05 11:48:03 -0300

    tentativa 1

--- led_shifter.v
+++ led_shifter.v
@@ -5,6 +5,23 @@ module LedShifter #(
     input  wire rst_n,
     output  reg [7:0] leds
 );
+    //
+    localparam CLK = CLK_FREQ - 1;
 
-    
+    reg [31:0] counter;
+
+    always @(posedge clk or negedge rst_n) begin
+        if (!rst_n) begin
+            counter <= 32'b0;
+            leds    <= 8'b00011111;
+        end else begin
+            if (counter >= CLK_FREQ) begin
+                leds    <= {leds[6:0], leds[7]};
+                counter <= 32'b0;
+            end else begin
+                counter <= counter+1;
+            end
+        end
+    end
+
 endmodule
